2b9c4ea78c6d6c7e989f758550cd7c5b505a7c07
Dag Arne Osvik <58537287+daosvik@users.noreply.github.com>
2024-09-05 19:36:44 +0200

    Add project files.

--- src/tt_um_daosvik_aesinvsbox.v
+++ src/tt_um_daosvik_aesinvsbox.v
@@ -1,11 +1,11 @@
 /*
- * Copyright (c) 2024 Your Name
+ * Copyright (c) 2024 Dag Arne Osvik
  * SPDX-License-Identifier: Apache-2.0
  */
 
 `default_nettype none
 
-module tt_um_example (
+module tt_um_daosvik_aesinvsbox (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
@@ -16,12 +16,14 @@ module tt_um_example (
     input  wire       rst_n     // reset_n - low to reset
 );
 
+  // Instantiate the inverse s-box module
+
+  sbox_aesinv sbox_aesinv_inst (.clk(clk), .x(ui_in), .y(uo_out), .cy(uio_out));
+
   // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
-  assign uio_oe  = 0;
+  assign uio_oe  = 255;
 
   // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
+  wire _unused = &{ena, rst_n, uio_in, 1'b0};
 
 endmodule
